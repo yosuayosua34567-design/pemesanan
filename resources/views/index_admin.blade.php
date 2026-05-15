84bfefbcea6859a42b5fb9a2c6e4cd00543d51c0
yosuayosua34567-design <yosuayosua34567@gmail.com>
2026-05-15 22:17:12 +0700

    Add files via upload

---
+++ resources/views/index_admin.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.base_admin')
+@section('container')
+
+    @if(\Session::has('alert'))
+        <?php
+        $alert = Session::get('alert');
+		echo "
+		<script type='text/javascript'>
+			alert('$alert');
+		</script>
+		";
+        ?>
+    @endif
+
+    <!-- Contact Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-5">Login Administrator User</h1>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-12">
+                    <div class="wow fadeInUp" data-wow-delay="0.2s">
+                        <form method="post" action="/login_admin">
+                            @csrf
+                            <div class="row g-3">
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input type="text" class="form-control" name="user_name" placeholder="Input Email User">
+                                        <label for="email">User ID</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input type="password" class="form-control" name="user_pass" placeholder="Your Name">
+                                        <label for="name">Kata Sandi</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100 py-3" type="submit">Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+@endsection
