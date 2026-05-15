889579e2dafed937bbbb3085cfc04c1d508b9788
yosuayosua34567-design <yosuayosua34567@gmail.com>
2026-05-15 21:57:56 +0700

    Add files via upload

---
+++ database/migrations/2024_06_16_041545_create_mechanics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mechanics', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 50);
+            $table->string('kata_sandi', 10);
+            $table->string('no_hp', 20);
+            $table->string('gambar', 255);
+            $table->text('alamat');
+            $table->string('jenis_kelamin', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mechanics');
+    }
+};
---
+++ database/migrations/2024_06_16_041601_create_schedules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_booking')->unique();
+            $table->bigInteger('kode_mekanik');
+            $table->string('hari', 15);
+            $table->time('jam_mulai');
+            $table->string('email_customer', 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
